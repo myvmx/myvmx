4ea5b39e5613c8a2899a8b8065f59a97c1a206bf
myVMX <myvmx@users.noreply.github.com>
2017-12-13 22:09:14 +0000

    Create SuppressShellWarning.ps1

---
+++ VMware/SuppressShellWarning.ps1
@@ -0,0 +1,11 @@
+#### Variables ####
+$ESXHost = "Your ESXi Host Name or IP Address Here" #Put your ESXi host name or IP address
+
+#Connect to your ESX Host
+Connect-viserver $ESXHost
+#Get the current setting
+Get-VMhost $ESXHost | Get-AdvancedSetting - Name UserVars.SuppressShellWarning 
+#Set the value to 1 and suppress confirmation of running this command
+Get-VMhost $ESXHost | Get-AdvancedSetting - Name UserVars.SuppressShellWarning | Set-AdvancedSetting -Value 1 -Confirm:$False
+#Show the setting
+Get-VMhost $ESXHost | Get-AdvancedSetting - Name UserVars.SuppressShellWarning 
