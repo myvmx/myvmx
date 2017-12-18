78fe0cc7150e5f20ce7ce802f321efc5c2ea5973
myVMX <myvmx@users.noreply.github.com>
2017-12-18 22:48:40 +0000

    Syntax Error -Name

--- VMware/SuppressShellWarning.ps1
+++ VMware/SuppressShellWarning.ps1
@@ -4,8 +4,8 @@ $ESXHost = "Your ESXi Host Name or IP Address Here" #Put your ESXi host name or 
 #Connect to your ESX Host
 Connect-viserver $ESXHost
 #Get the current setting
-Get-VMhost $ESXHost | Get-AdvancedSetting - Name UserVars.SuppressShellWarning 
+Get-VMhost $ESXHost | Get-AdvancedSetting -Name UserVars.SuppressShellWarning 
 #Set the value to 1 and suppress confirmation of running this command
-Get-VMhost $ESXHost | Get-AdvancedSetting - Name UserVars.SuppressShellWarning | Set-AdvancedSetting -Value 1 -Confirm:$False
+Get-VMhost $ESXHost | Get-AdvancedSetting -Name UserVars.SuppressShellWarning | Set-AdvancedSetting -Value 1 -Confirm:$False
 #Show the setting
-Get-VMhost $ESXHost | Get-AdvancedSetting - Name UserVars.SuppressShellWarning 
+Get-VMhost $ESXHost | Get-AdvancedSetting -Name UserVars.SuppressShellWarning 
