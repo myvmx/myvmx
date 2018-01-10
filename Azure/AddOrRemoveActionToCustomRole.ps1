6dbf276df1a59e837c0b7997a852db20fbcbac36
myVMX <myvmx@users.noreply.github.com>
2018-01-10 14:04:26 +0000

    Update AddOrRemoveActionToCustomRole.ps1

--- Azure/AddOrRemoveActionToCustomRole.ps1
+++ Azure/AddOrRemoveActionToCustomRole.ps1
@@ -1,29 +1,29 @@
 ﻿<####
-# Script to get add actions to a custom role
+# Script to get add/remove actions to a custom role
 # 
 # KY 27/12/17 v0.1
 ####>
 
-##Variable
-#define the tag you would like to find
+### Variable
+## define the tag you would like to find
 $SubId = "You SubSciption ID"
-#Custom Role Name in Azure
+##C ustom Role Name in Azure
 $CustomRoleName = "Your Custom Role" 
-##
+###
 
-#Login to Azure
+## Login to Azure
 Login-AzureRmAccount
-#Select the Azure account
+## Select the Azure account
 Select-AzureRmSubscription -SubscriptionId $SubId
 
-# Get the role information and store as variable
+## Get the role information and store as variable
 $role = Get-AzureRmRoleDefinition $CustomRoleName
-# Add the action you would like to the role
+## Add the action/s you would like to the role
 $Role.Actions.Add("Microsoft.Compute/virtualMachines/performMaintenance/action")
-# To remove an action
+## To remove an action
 #$Role.Actions.Remove("Microsoft.Compute/virtualMachines/Redeploy/action")
-# Commit the change
+## Commit the change
 Set-AzureRmRoleDefinition -Role $role
-# Display the role permissions
+## Display the role permissions
 (Get-AzureRMRoleDefinition -Name $CustomRoleName).Actions
 
