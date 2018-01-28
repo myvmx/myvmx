061e3f1080db7277768fccdd2fc339a6cccdd56e
myVMX <myvmx@users.noreply.github.com>
2018-01-28 22:57:16 +0000

    Update CreateiSCSISwitch.ps1
    Reformatted the script

--- VMware/CreateiSCSISwitch.ps1
+++ VMware/CreateiSCSISwitch.ps1
@@ -1,83 +1,82 @@
-### This script is to create an new vSwitch
-### Create all the vmkernel 
-### enable iSCSI
-### create port binding
-
-
+<#
+This script is to create an new vSwitch
+Create all the vmkernel enable iSCSI
+create port binding
+#>
 
 ### Variables for creating the iSCSI Switch
-$ESXHostName = "IP or host name of ESXi"
-$iSCSISwName = "Name you would like for vSwitch ie vSwitch1" #iSCSI switch name
-$iSCSIMTU = "MTU size you will use for iSCSI" #iSCSI MTU Size
-$iSCSIPg1Name = "Portgroup Name for iSCSI 1 connection" #iSCSI 1 Portgroup Name
-$iSCSIPg2Name = "Portgroup Name for iSCSI 2 connection" #iSCSI 2 Portgroup Name
-$iSCSIPhyNic1 = "Physical Adapter Nic Name for iSCSI 1" #iSCSI 1 Physical Adapter Nic Name
-$iSCSIPhyNic2 = "Physical Adapter Nic Name for iSCSI 1" #iSCSI 2 Physical Adapter Nic Name
-$iSCSIIP1 = "IP Address for iSCSI 1 VMKernel" #iSCSI 1 IP address for vmkernel
-$iSCSIIP2 =  "IP Address for iSCSI 2 VMKernel" #iSCSI 2 IP address for vmkernel
-$iSCSISubnet = "Subnet IP for both iSCSI VMKernel" #iSCSI Adapter Subnet
-$iSCSItargetIP = "iSCSI Server Target IP Address" #iSCSI Server Target IP
+## The following three should change for each host you deploy
+$ESXHostName = "You ESXi Host IP or FQDN"
+$iSCSIIP1 = "IP Address for iSCSI VMKernel adapter 1" #iSCSI 1 IP address for vmkernel
+$iSCSIIP2 =  "IP Address for iSCSI VMKernel adapter 2" #iSCSI 2 IP address for vmkernel
+##  The remaining variables should be the same across your host/cluster/datacentre
+$iSCSISubnet = "Subnet for iSCSI VMKernel adapter" #iSCSI Adapter Subnet
+$iSCSISwName = "Name you want to give for vSwitch" #iSCSI switch name
+$iSCSIMTU = "MTU Size for iSCSI" #iSCSI MTU Size
+$iSCSIPg1Name = "Port Group 1 name" #iSCSI 1 Portgroup Name
+$iSCSIPg2Name = "Port Group 2 name" #iSCSI 2 Portgroup Name
+$iSCSIPhyNic1 = "Your physical Adapter Nic 1 Name" #iSCSI 1 Physical Adapter Nic Name
+$iSCSIPhyNic2 = "Your physical Adapter Nic 2 Name" #iSCSI 2 Physical Adapter Nic Name
+$iSCSItargetIP = "IP address of your iSCSI Array" #iSCSI Server Target IP
+
 ##################################################################
 
-#Connect to Host
+## Connect to Host
 Connect-VIServer $ESXHostName
-
-#Store Host detail as a variable to use through the script
-$ESXHost = Get-VMhost 
-
-#Put host into maintenance
-$ESXHost | Set-VMHost -State "Maintenance"
-
-#Create iSCSI Switch
-New-VirtualSwitch -VMHost $ESXHost -Name $iSCSISwName -Mtu $iSCSIMTU -confirm:$false
-
-#Create the two VMKernel iSCSI 
-New-VMHostNetworkAdapter -vmhost $ESXHost -VirtualSwitch $iSCSISwName -Portgroup $iSCSIPg1Name -IP $iSCSIIP1 -SubnetMask $iSCSISubnet -mtu $iSCSIMTU
-New-VMHostNetworkAdapter -vmhost $ESXHost -VirtualSwitch $iSCSISwName -Portgroup $iSCSIPg2Name -IP $iSCSIIP2 -SubnetMask $iSCSISubnet -mtu $iSCSIMTU
-
-#Add Physical Nic to iSCSI Switch
-$VMHostPhysicalNic1 = Get-vmhost $ESXHost | Get-VMHostNetworkAdapter -Physical -Name $iSCSIPhyNic1
-get-virtualSwitch -vmhost $ESXHost -name $iSCSISwName | Add-VirtualSwitchPhysicalNetworkAdapter -VMHostPhysicalNic $VMHostPhysicalNic1 -Confirm:$false
-$VMHostPhysicalNic2 = Get-vmhost $ESXHost | Get-VMHostNetworkAdapter -Physical -Name $iSCSIPhyNic2
-get-virtualSwitch -vmhost $ESXHost -name $iSCSISwName | Add-VirtualSwitchPhysicalNetworkAdapter -VMHostPhysicalNic $VMHostPhysicalNic2 -Confirm:$false
-get-virtualportgroup -VMHost $ESXHost -Name $iSCSIPg1Name | get-nicteamingpolicy | Set-NicTeamingPolicy -MakeNicActive $iSCSIPhyNic1 -MakeNicUnused $iSCSIPhyNic2
-get-virtualportgroup -VMHost $ESXHost -Name $iSCSIPg2Name | get-nicteamingpolicy | Set-NicTeamingPolicy -MakeNicActive $iSCSIPhyNic2 -MakeNicUnused $iSCSIPhyNic1
-
-#Get all adapters with VMK
-$vmks = Get-VMHostNetworkAdapter | where {$_.name -like "vmk*"} #Get all vmks
-
-#Set iSCSI software adapter to be enabled
-Get-vmhoststorage $ESXHost | set-vmhoststorage -SoftwareIScsiEnabled $true
-
+## Put host into maintenance
+Set-VMHost -State "Maintenance"
+## Create iSCSI Switch
+New-VirtualSwitch -Name $iSCSISwName -Mtu $iSCSIMTU -confirm:$false
+## Add physical nic 1 to the Switch
+$VMHostPhysicalNic1 = Get-vmhost | Get-VMHostNetworkAdapter -Physical -Name $iSCSIPhyNic1
+get-virtualSwitch -name $iSCSISwName | Add-VirtualSwitchPhysicalNetworkAdapter -VMHostPhysicalNic $VMHostPhysicalNic1 -Confirm:$false
+## Add physical nic 2 to the Switch
+$VMHostPhysicalNic2 = Get-vmhost | Get-VMHostNetworkAdapter -Physical -Name $iSCSIPhyNic2
+get-virtualSwitch -name $iSCSISwName | Add-VirtualSwitchPhysicalNetworkAdapter -VMHostPhysicalNic $VMHostPhysicalNic2 -Confirm:$false
+## Modify vSwitch Policies
+Get-VirtualSwitch -Name $iSCSISwName | Get-SecurityPolicy | Set-SecurityPolicy -AllowPromiscuous $false -MacChanges $true -ForgedTransmits $true -confirm:$false
+Get-VirtualSwitch -Name $iSCSISwName | Get-NicTeamingPolicy | Set-NicTeamingPolicy -MakeNicActive $iSCSIPhyNic1,$iSCSIPhyNic2
+Get-VirtualSwitch -Name $iSCSISwName | Get-NicTeamingPolicy | Set-NicTeamingPolicy -LoadBalancingPolicy LoadBalanceSrcId
+Get-VirtualSwitch -Name $iSCSISwName | Get-NicTeamingPolicy | Set-NicTeamingPolicy -NetworkFailoverDetectionPolicy LinkStatus
+Get-VirtualSwitch -Name $iSCSISwName | Get-NicTeamingPolicy | Set-NicTeamingPolicy -FailbackEnabled:$true -NotifySwitches:$true 
+## Create the two VMKernel iSCSI 
+New-VMHostNetworkAdapter -VirtualSwitch $iSCSISwName -Portgroup $iSCSIPg1Name -IP $iSCSIIP1 -SubnetMask $iSCSISubnet -mtu $iSCSIMTU
+New-VMHostNetworkAdapter -VirtualSwitch $iSCSISwName -Portgroup $iSCSIPg2Name -IP $iSCSIIP2 -SubnetMask $iSCSISubnet -mtu $iSCSIMTU
+## Modify 1st Port Group for iSCSI
+get-virtualportgroup -Name $iSCSIPg1Name | get-nicteamingpolicy | Set-NicTeamingPolicy -MakeNicActive $iSCSIPhyNic1 -MakeNicUnused $iSCSIPhyNic2
+Get-Virtualportgroup -Name $iSCSIPg1Name | Get-SecurityPolicy | Set-SecurityPolicy -AllowPromiscuousInherited $true -ForgedTransmitsInherited $true -MacChangesInherited $true -confirm:$false 
+get-virtualportgroup -Name $iSCSIPg1Name | get-nicteamingpolicy | Set-NicTeamingPolicy -InheritLoadBalancingPolicy:$true -InheritNetworkFailoverDetectionPolicy:$true
+get-virtualportgroup -Name $iSCSIPg1Name | get-nicteamingpolicy | Set-NicTeamingPolicy -InheritNotifySwitches:$true -InheritFailback:$true -InheritFailoverOrder:$false
+## Modify 2nd Port Group for iSCSI
+get-virtualportgroup -Name $iSCSIPg2Name | get-nicteamingpolicy | Set-NicTeamingPolicy -MakeNicActive $iSCSIPhyNic2 -MakeNicUnused $iSCSIPhyNic1
+Get-Virtualportgroup -Name $iSCSIPg2Name | Get-SecurityPolicy | Set-SecurityPolicy -AllowPromiscuousInherited $true -ForgedTransmitsInherited $true -MacChangesInherited $true -confirm:$false 
+get-virtualportgroup -Name $iSCSIPg2Name | get-nicteamingpolicy | Set-NicTeamingPolicy -InheritLoadBalancingPolicy:$true -InheritNetworkFailoverDetectionPolicy:$true
+get-virtualportgroup -Name $iSCSIPg2Name | get-nicteamingpolicy | Set-NicTeamingPolicy -InheritNotifySwitches:$true -InheritFailback:$true -InheritFailoverOrder:$false
+## Set iSCSI software adapter to be enabled
+Get-vmhoststorage | set-vmhoststorage -SoftwareIScsiEnabled $true
 #Let the script sleep for 30 seconds so that the host can enable the iSCSI adapter before moving on
 Write-Host "Sleeping for 30 Seconds..." -ForegroundColor Green
 Start-Sleep -Seconds 30
-
-#Get the iSCSI hba details to be used later
-$hba = $ESXHost | Get-VMHostHba -Type iScsi | Where {$_.Model -eq "iSCSI Software Adapter"}
-
-#Create the target
+## Get all adapters where the name has VMK
+$vmks = Get-VMHostNetworkAdapter | where {$_.name -like "vmk*"} #Get all vmks
+## Get the iSCSI hba details to be used later
+$hba = Get-vmhost | Get-VMHostHba -Type iScsi | Where {$_.Model -eq "iSCSI Software Adapter"}
+## Create the target ie the connection to your array
 New-IScsiHbaTarget -IScsiHba $hba -Address $iSCSItargetIP
-
-# To get the vmk name so that we can use it to do iSCSI binding
+## We need to get the vmk name that are attached to the iSCSI portgroup
+## so that we can do the iSCSI binding. Store them in vmkscsi variable to use
 ForEach ($vmk in $vmks){If ($vmk.PortGroupName -eq $iSCSIPg1Name) {$vmkscsi01 = $vmk.DeviceName}}
 ForEach ($vmk in $vmks){If ($vmk.PortGroupName -eq $iSCSIPg2Name) {$vmkscsi02 = $vmk.DeviceName}}
-
-#iSCSI port binding we need to use esxcli
+## iSCSI port binding we need to use esxcli
 $esxcli = Get-EsxCli -VMHost $ESXHost -v2
- 
-#Binds VMKernel ports created earlier to the iSCSI Software Adapter HBA
+## Binds VMKernel ports created earlier to the iSCSI Software Adapter HBA
 $esxcli.iscsi.networkportal.add.invoke(@{adapter = $hba.device;nic = $vmkscsi01})
 $esxcli.iscsi.networkportal.add.invoke(@{adapter = $hba.device;nic = $vmkscsi02})
-
-#Use this information to pass on to your san to carry on
+## Use this information to pass on to your san/array to carry on 
 $hba.iscsiname 
-
-#Scan the host so that it picks up the new LUNS that has already been assigned to it
+## Scan the host so that it picks up the new LUNS that has already been assigned to it
 Get-VMHostStorage -VMHost $ESXHost -RescanAllHba
 Write-Host "Sleeping for 30 Seconds to let the host scan and pick up all the luns ..." -ForegroundColor Green
 Start-Sleep -Seconds 30
-
-Disconnect-VIServer $ESXhostNameFQDN -Force -Confirm:$false
-
-
+## Disconnect from ESXi Server
+Disconnect-VIServer $ESXHostName -Force -Confirm:$false
