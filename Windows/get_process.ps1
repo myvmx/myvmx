e30d615fe957646877fbcc173e32d3bc2fabfd62
myVMX <myvmx@users.noreply.github.com>
2020-06-09 16:57:10 +0100

    Update get_process.ps1

--- Windows/get_process.ps1
+++ Windows/get_process.ps1
@@ -2,3 +2,7 @@ get-process
 <#
 $ScriptFromGitHub = Invoke-WebRequest https://raw.githubusercontent.com/myvmx/myvmx/master/Windows/get_process.ps1
 Invoke-Expression $($ScriptFromGitHub.Content)#>
+
+
+<#
+Invoke-WebRequest https://raw.githubusercontent.com/myvmx/myvmx/master/Windows/get_process.ps1  -OutFile c:\stuff\get_process.ps1#>
