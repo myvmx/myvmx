8bafd38cc634256807ac57365671793d900afe70
myVMX <myvmx@users.noreply.github.com>
2022-09-14 13:53:44 +0100

    Create GetStorageAccountCapacity.ps1

---
+++ Azure/GetStorageAccountCapacity.ps1
@@ -0,0 +1,34 @@
+# To get storage account capacity average usage over a period of time, very rough output
+
+$subs = Get-AzSubscription
+ foreach ($sub in $subs)
+ {
+    Set-AzContext -Subscription $sub.Name
+    $StorageAccounts = Get-AzResource -ResourceType "Microsoft.Storage/storageAccounts"
+        foreach ($StorageAccount in $StorageAccounts)
+            {
+
+                #Start-Sleep -Milliseconds 300
+                $StorInfo = Get-AzStorageAccount -ResourceGroupName $StorageAccount.ResourceGroupName -Name $StorageAccount.Name
+                $usedCapacity = (Get-AzMetric -ResourceId $StorInfo.Id -MetricName "UsedCapacity" -StartTime (get-date).adddays(-1) -EndTime (Get-Date)).Data
+                $usedCapacityInMB = $usedCapacity.Average | select -first 1 
+                $usedCapacityInMB = $usedCapacityInMB / 1024 / 1024
+                
+                $StorageAccountName = $StorageAccount.resourceName
+                $StorageRGName = $StorageAccount.ResourceGroupName
+                $SubName = $sub.Name
+                $Storkind = $StorInfo.kind
+                $Stortier = $storinfo.AccessTier
+                $storskuname = $StorInfo.Sku.name
+                $storskuperformance = $storinfo.sku.tier
+                "$StorageAccountName,$usedCapacityInMB,$StorageRGName,$SubName,$storkind, $stortier, $storskuname, $storskuperformance" >> ".\storageAccountsUsedCapacityiPSLDev.csv"
+                $StorageAccountName
+                $usedCapacityInMB
+                $subname
+                $storkind
+                $stortier
+                $storskuname
+                $storskuperformance
+
+            }
+}
